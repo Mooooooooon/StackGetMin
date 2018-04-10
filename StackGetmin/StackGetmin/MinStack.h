6d79fda298c33a9bf83dd1e07f853423ecf2de24
Moon_C <cms647218@gmail.com>
2018-04-10 17:20:54 -0400

    dd

--- StackGetmin/StackGetmin/MinStack.h
+++ StackGetmin/StackGetmin/MinStack.h
@@ -3,26 +3,17 @@
 
 class MinStack {
 private:
-
 	int *p; // pointer to main stack
 	int idx; // index
 	int length; // size of the stack
 	int *mins; // pointer to minimum stack
 
-
-
 public:
 	MinStack(int size);
-
 	void push(int num); // insert value to the top
 	void pop(); // deletes the top value
 	int top(); // returns the top value
 	void displayTop(); // prints the top value
 	int min(); // returns the minimum value
 	void displayMin(); // prints the min value
-
-
-
-
-
 };
